7bb08fa92a16345b009d6a502e84f6e48bddd4e1
Galineras <74051615+Galineras@users.noreply.github.com>
2020-11-24 13:48:06 +0100

    tables groupe et membre

--- Etape_2/creation_base.sql
+++ Etape_2/creation_base.sql
@@ -11,11 +11,32 @@ CREATE TABLE artiste (
 	dateMort date,
 	CONSTRAINT uniqueNomPreNai UNIQUE (nomA, prenom, dateNais),
 	CONSTRAINT dateNaisMort CHECK (dateNais < dateMort)
-);
+)
 
 
+CREATE TABLE groupe (
+	idG int PRIMARY KEY,
+	nomG varchar(50) NOT NULL,
+	dateCrea date NOT NULL,
+	nationG varchar(50),
+	genre varchar(50) NOT NULL
+)
 
 
+CREATE TABLE membre (
+	idMe int PRIMARY KEY,
+	role varchar(50) NOT NULL,
+	dateDeb date NOT NULL,
+	dateFin date,
+	idA int NOT NULL,
+	idG int NOT NULL,
+	CONSTRAINT dateDebFin CHECK (dateDeb < dateFin),
+	FOREIGN KEY (idA) REFERENCES artiste(idA)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idG) REFERENCES groupe(idG)
+	ON DELETE CASCADE ON UPDATE CASCADE
+)
 
 
+;
 /*---------------------------------------------*/
