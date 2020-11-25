6756ef414023e33eda558313a612f29d2f7f9816
Galineras <74051615+Galineras@users.noreply.github.com>
2020-11-25 13:08:13 +0100

    remplissage fictif (INSERTs)

--- Etape_2/creation_base.sql
+++ Etape_2/creation_base.sql
@@ -121,4 +121,66 @@ CREATE TABLE historique (
 	CONSTRAINT historique_PK PRIMARY KEY (pseudo, idMo, dateHeure)
 );
 
-/*---------------------------------------------*/
+/*---------------Pour-meubler-----------------*/
+
+/* utilisateurs */
+INSERT INTO utilisateur VALUES ('admin_TheWave', 'admin.thewave@mailpro.fr', '2019-11-30', 'uZ3rFr13NdlY');
+INSERT INTO utilisateur VALUES ('Galineras', 'gali.neras@mail.fr', '2020-11-25', 'topsecretmdp');
+INSERT INTO utilisateur VALUES ('BananaSplit', 'banana.split@mail.fr', '2020-11-25', '42finalanswer');
+INSERT INTO utilisateur VALUES ('JeanKevin69', 'jean.kevin@mail.fr', '2020-11-25', 'tupeuxpastest');
+INSERT INTO utilisateur VALUES ('CpasGrave', 'david.goodenough@mail.fr', '2020-11-26', '0000');
+
+/* groupes */
+INSERT INTO groupe VALUES (1, 'The Beatles', '1960-08-15', 'britannique', 'pop rock');
+INSERT INTO groupe VALUES (2, 'Santa Claws', '1982-12-25', 'americain', 'pop');
+
+/* artistes */
+INSERT INTO artiste VALUES (1, 'Lennon', 'John', 'britannique', '1940-10-09', '1980-12-08');
+INSERT INTO artiste VALUES (2, 'McCartney', 'Paul', 'britannique', '1942-06-18');
+INSERT INTO artiste VALUES (3, 'Harrison', 'George', 'britannique', '1943-02-25', '2001-11-29');
+INSERT INTO artiste VALUES (4, 'Starkey', 'Richard', 'britannique', '1940-07-07');
+INSERT INTO artiste VALUES (5, 'Claus', 'Santa', 'americain', '0001-01-01');
+INSERT INTO artiste VALUES (6, 'Rudolph', 'Ren', NULL, '0001-01-01');
+INSERT INTO artiste VALUES (7, 'Simon', 'Elf', 'elfeïmais', '0001-01-01');
+
+/* membres */
+INSERT INTO membre VALUES (1, 'guitariste', '1960-08-15', '1969-09-20', 1, 1);
+INSERT INTO membre VALUES (2, 'chanteur', '1960-08-15', '1969-09-20', 1, 1);
+INSERT INTO membre VALUES (3, 'bassiste', '1960-08-15', '1969-04-07', 2, 1);
+INSERT INTO membre VALUES (4, 'guitariste', '1960-08-15', '1969-01-10', 3, 1);
+INSERT INTO membre VALUES (5, 'batteur', '1962-01-01', '1968-08-22', 4, 1);
+INSERT INTO membre VALUES (6, 'chanteur', '0001-01-01', '0001-12-25', 5, 2);
+INSERT INTO membre VALUES (7, 'batteur', '0001-12-25', NULL, 5, 2);
+INSERT INTO membre VALUES (8, 'claviériste', '0001-12-25', NULL, 6, 2);
+INSERT INTO membre VALUES (9, 'chanteur', '0001-12-25', NULL, 7, 2);
+INSERT INTO membre VALUES (10, 'danseur', '0001-12-25', NULL, 7, 2);
+
+/* morceaux */
+INSERT INTO morceau VALUES (1, 'We wish you a merry christmas', '00:05:31', 'www.wish-you.com/merry_christmas/lyrics', 'www.wish-you.com/merry_christmas/audio', 2);
+INSERT INTO morceau VALUES (2, 'Ho ho ho', '00:02:54', 'www.wish-you.com/ho_ho_ho/lyrics', 'www.wish-you.com/ho_ho_ho/audio', 2);
+INSERT INTO morceau VALUES (3, 'He is coming to town', '00:04:04', 'www.wish-you.com/coming_town/lyrics', 'www.wish-you.com/coming_town/audio', 2);
+INSERT INTO morceau VALUES (4, 'Let it be', '00:03:46', 'https://www.youtube.com/watch?v=_wYyWA6ZdVU', 'https://www.youtube.com/watch?v=_wYyWA6ZdVU', 1);
+INSERT INTO morceau VALUES (5, 'Hey Jude', '00:04:33', 'https://www.youtube.com/watch?v=7qMls5yxP1w', 'https://www.youtube.com/watch?v=7qMls5yxP1w', 1);
+INSERT INTO morceau VALUES (6, 'Yellow submarine', '00:02:37', 'https://www.youtube.com/watch?v=j_JaDDcyIIU', 'https://www.youtube.com/watch?v=j_JaDDcyIIU', 1);
+INSERT INTO morceau VALUES (7, 'Yesterday', '00:02:05', 'https://www.youtube.com/watch?v=jo505ZyaCbA', 'https://www.youtube.com/watch?v=jo505ZyaCbA', 1);
+INSERT INTO morceau VALUES (8, 'Hello, goodbye', '00:03:07', 'https://www.youtube.com/watch?v=BD75RTV9P5w', 'https://www.youtube.com/watch?v=BD75RTV9P5w', 1);
+
+/* participations */
+INSERT INTO participe VALUES (5, 1);
+INSERT INTO participe VALUES (6, 1);
+INSERT INTO participe VALUES (7, 1);
+INSERT INTO participe VALUES (5, 2);
+INSERT INTO participe VALUES (5, 3);
+INSERT INTO participe VALUES (6, 3);
+
+
+
+
+
+
+
+
+
+
+
+
