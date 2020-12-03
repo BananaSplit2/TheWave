e4c3f8475b579e5f6e030486699a22cf3d390a0d
BananaSplit2 <51517972+BananaSplit2@users.noreply.github.com>
2020-12-03 02:09:45 +0100

    Fix

--- Etape_3/inc/connexiondb.php
+++ Etape_3/inc/connexiondb.php
@@ -7,6 +7,6 @@ try {
     $db = new PDO('pgsql:host=localhost;dbname=thewave', $user, $pass);
 }
 catch(PDOException $e) {
-    print "Erreur :" . $e.getMessage() . '<br>';
+    print "Erreur :" . $e->getMessage() . '<br>';
     die();
 }
