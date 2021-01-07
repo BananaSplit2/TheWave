43a70e130f62085d874718f66b3f3fa2d50d0ea8
BananaSplit2 <51517972+BananaSplit2@users.noreply.github.com>
2021-01-07 05:49:26 +0100

    page de profil + page d'accueil

--- Etape_3/index.php
+++ Etape_3/index.php
@@ -16,11 +16,64 @@ require("inc/connexiondb.inc.php");
         echo '<div class="alert alert-primary" role="alert">Vous vous êtes déconnecté.</div>';
     }
     ?>
+    <div class="jumbotron">
+        <h1 class="display-4">The Wave</h1>
+        <p class="lead">Bienvenue sur la plateforme de streaming audio The Wave.</p>
+        <hr class="my-2">
+        <p>La musique n'attend que vous...</p>
+    </div>
     <div class="row">
-        <div class="col text-center">
-            <h1>Accueil (bien vide ma foi)</h1>
-            <a href="album.php?idal=1">Pourquoi ne pas regarder cet album ?</a>
+        <div class="col-6">
+            <h4>Morceaux les plus écoutés de la semaine</h4>
+            <table class="table table-sm table-striped">
+                <tr><th>Titre</th><th>Nombre d'écoutes</th></tr>
+                <?php
+                $meilleures_ecoutes = $db->query("SELECT idmo, titrem, count(*) as ecoutes FROM historique
+                                                NATURAL JOIN morceau
+                                                WHERE age(dateheure) <= interval '7 days'
+                                                GROUP BY idmo, titrem
+                                                ORDER BY ecoutes DESC
+                                                LIMIT 10;");
+                foreach ($meilleures_ecoutes as $morceau) {
+                    echo '<tr><td><a href="morceau.php?idmo='. $morceau['idmo'] .'">'. $morceau['titrem'] .'</a></td>';
+                    echo '<td>'. $morceau['ecoutes'] . '</td></tr>';
+                }
+                ?>
+            </table>
         </div>
+        <div class="col-6">
+            <h4>Groupes les plus suivis</h4>
+            <table class="table table-sm table-striped">
+                <tr><th>Groupe</th><th>Nombre d'utilisateurs les suivant</th></tr>
+                <?php
+                $groupes = $db->query("SELECT idg, nomg, count(pseudo) as followers FROM suitgroupe
+                                            NATURAL JOIN groupe
+                                            GROUP BY idg, nomg
+                                            ORDER BY followers DESC
+                                            LIMIT 10;");
+                foreach ($groupes as $groupe) {
+                    echo '<tr><td><a href="groupe.php?idg='. $groupe['idg'] .'">'. $groupe['nomg'] .'</a></td>';
+                    echo '<td>'. $groupe['followers'] .'</td></tr>';
+                }
+                ?>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <h4>Derniers albums</h4>
+        <table class="table table-sm table-striped">
+            <tr><th>Album</th><th>Groupe</th><th>Date de sortie</th></tr>
+            <?php
+            $albums = $db->query("SELECT idal, titrea, dateparu, idg, nomg FROM album NATURAL JOIN groupe
+                                            ORDER BY dateparu DESC
+                                            LIMIT 10;");
+            foreach ($albums as $album) {
+                echo '<tr><td><a href="album.php?idal='. $album['idal'] .'">'. $album['titrea'] .'</a></td>';
+                echo '<td><a href="groupe.php?idg='. $album['idg'] .'">'. $album['nomg'] .'</td>';
+                echo '<td>'. $album['dateparu'] .'</td></tr>';
+            }
+            ?>
+        </table>
     </div>
 </main>
 
--- Etape_3/profil.php
+++ Etape_3/profil.php
@@ -7,9 +7,124 @@ require("inc/header.inc.php");
 ?>
 
 <main class="container">
+    <div class="row my-4">
+        <div class="col">
+            <h1>Page de profil</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <h4>Morceaux les plus écoutés</h4>
+            <table class="table table-sm table-striped">
+                <tr><th>Titre</th><th>Nombre d'écoutes</th></tr>
+                <?php
+                $meilleures_ecoutes = $db->prepare("SELECT idmo, titrem, count(*) as ecoutes FROM historique
+                                                NATURAL JOIN morceau
+                                                WHERE pseudo = :pseudo
+                                                GROUP BY idmo, titrem
+                                                ORDER BY ecoutes DESC
+                                                LIMIT 10;");
+                $meilleures_ecoutes->bindParam(":pseudo", $_SESSION['pseudo']);
+                $meilleures_ecoutes->execute();
+                foreach ($meilleures_ecoutes as $morceau) {
+                    echo '<tr><td><a href="morceau.php?idmo='. $morceau['idmo'] .'">'. $morceau['titrem'] .'</a></td>';
+                    echo '<td>'. $morceau['ecoutes'] . '</td></tr>';
+                }
+                ?>
+            </table>
+        </div>
+        <div class="col-6">
+            <h4>Historique des dernières écoutes</h4>
+            <table class="table table-sm table-striped">
+                <tr><th>Titre</th><th>Date d'écoute</th></tr>
+                <?php
+                $historique = $db->prepare("SELECT idmo, titrem, dateheure FROM historique NATURAL LEFT JOIN morceau
+                                                            WHERE pseudo = :pseudo
+                                                            ORDER BY dateheure DESC
+                                                            LIMIT 10;");
+                $historique->bindParam(":pseudo", $_SESSION['pseudo']);
+                $historique->execute();
+                foreach ($historique as $morceau) {
+                    echo '<tr><td><a href="morceau.php?idmo='. $morceau['idmo'] .'">'. $morceau['titrem'] .'</a></td>';
+                    echo '<td>'. $morceau['dateheure'] . '</td></tr>';
+                }
+                ?>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <h4>Activité des comptes suivis</h4>
+            <table class="table table-sm table-sI witriped">
+                <tr><th>Playlist</th><th>Utilisateur</th><th>Date de dernière modification</th></tr>
+                <?php
+                $playlists = $db->prepare("SELECT idp, titre, pseudo, datemodif FROM playlist
+                                                    WHERE pseudo IN (
+                                                        SELECT suivi FROM suitutilisateur
+                                                        WHERE suit = :pseudo
+                                                        )
+                                                    AND privee = False
+                                                    ORDER BY datemodif DESC
+                                                    LIMIT 10;");
+                $playlists->bindParam(":pseudo", $_SESSION['pseudo']);
+                $playlists->execute();
+
+                foreach ($playlists as $playlist) {
+                    echo '<tr><td><a href="playlist.php?idp='. $playlist['idp'] .'">'. $playlist['titre'] .'</a></td>';
+                    echo '<td><a href="utilisateur.php?pseudo='. $playlist['pseudo'] .'">'. $playlist['pseudo'] .'</a></td>';
+                    echo '<td>'. $playlist['datemodif'] .'</td></tr>';
+                }
+                ?>
+            </table>
+        </div>
+        <div class="col-6">
+            <h4>Activité des groupes suivis</h4>
+            <table class="table table-sm table-striped">
+                <tr><th>Album</th><th>Groupe</th><th>Date de sortie</th></tr>
+                <?php
+                $albums = $db->prepare("SELECT idal, titrea, idg, nomg, dateparu FROM album NATURAL JOIN groupe
+                                                WHERE idg IN (
+                                                    SELECT idg FROM suitgroupe
+                                                    WHERE pseudo = :pseudo
+                                                    )
+                                                ORDER BY dateparu DESC
+                                                LIMIT 10;");
+                $albums->bindParam(":pseudo", $_SESSION['pseudo']);
+                $albums->execute();
+
+                foreach ($albums as $album) {
+                    echo '<tr><td><a href="album.php?idal='. $album['idal'] .'">'. $album['titrea'] .'</a></td>';
+                    echo '<td><a href="groupe.php?idg='. $album['idg'] .'">'. $album['nomg'] .'</a></td>';
+                    echo '<td>'. $album['dateparu'] .'</td></tr>';
+                }
+                ?>
+            </table>
+
+        </div>
+    </div>
     <div class="row">
         <div class="col">
-            <h1>Page de profile</h1>
+            <h4>Playlists personnelles</h4>
+            <table class="table table-sm table-striped">
+                <tr><th>Titre</th><th>Accessibilité</th><th>Dernière modification</th><th></th></tr>
+                <?php
+                $playpersos = $db->prepare("SELECT idp, titre, privee, datemodif FROM playlist WHERE pseudo = :pseudo");
+                $playpersos->bindParam(":pseudo", $_SESSION['pseudo']);
+                $playpersos->execute();
+
+                foreach ($playpersos as $playlist) {
+                    if ($playlist['privee'] == false) {
+                        $access = "Publique";
+                    }
+                    else {
+                        $access = "Privée";
+                    }
+                    echo '<tr><td>'. $playlist['titre'] .'</td><td>'. $access .'</td><td>'. $playlist['datemodif'] .'</td>';
+                    echo '<td><a href="">Editer</a></td></tr>';
+                }
+
+                ?>
+            </table>
         </div>
     </div>
     <div class="row py-4">
